1467eb1686f4456a5282587d53bc30e4e52a9998
Micah Huff <micah@micahhuff.com>
2017-11-20 21:39:25 -0800

    Simple UI

---
+++ AirdropMod/Client_PresentMenuUI.lua
@@ -0,0 +1,27 @@
+require('Utilities');
+
+function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
+	Game = game; --make it globally accessible
+
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+
+	if (game.Us ~= nil) then --don't show propose button to spectators
+		UI.CreateButton(vert).SetText("Create Airdrop").SetOnClick(function()
+			game.CreateDialog(CreateAirdropDialog);
+		end);
+	end
+
+end
+
+function CreateAirdropDialog(rootParent, setMaxSize, setScrollable, game, close)
+	setMaxSize(390, 300);
+	TargetPlayerID = nil;
+
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+
+	
+	local row1 = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(row1).SetText("Select territory to airdrop to: ");
+
+end
+
