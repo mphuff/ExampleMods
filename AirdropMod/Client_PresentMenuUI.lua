29365eb5e96de555ba29a947f4dbf4e7878169c4
Micah Huff <micah@micahhuff.com>
2017-11-21 08:14:49 -0800

    Mimic GiftArmiesMod code

--- AirdropMod/Client_PresentMenuUI.lua
+++ AirdropMod/Client_PresentMenuUI.lua
@@ -3,34 +3,54 @@ require('Utilities');
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
 	Game = game; --make it globally accessible
 
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-
 	if (game.Us ~= nil) then --don't show airdrop button to spectators
-		standing = game.LatestStanding
-    if (standing ~= nil) then
-      print("Standing was NOT nil")
-      territories = standing.Territories;
-      Dump(territories);
-    else
-      print("Standing was nil");
-    end
+    local vert = UI.CreateVerticalLayoutGroup(rootParent);
+    local turnNumber = game.Game.TurnNumber;
+    local row1 = UI.CreateHorizontalLayoutGroup(vert);
+    UI.CreateLabel(row1).SetText("Airdrop armies to this territory: ");
+    TargetTerritoryBtn = UI.CreateButton(row1).SetText("Select source territory...").SetOnClick(TargetTerritoryClicked);
     
-		local turnNumber = game.Game.TurnNumber;
-    --Dump(turnNumber);
-		--openTerritories = filter(allTerritories, territoryIsNeutral);
-    --Dump(openTerritories);
 
-		local row1 = UI.CreateHorizontalLayoutGroup(vert);
-		--UI.CreateButton(row1).SetText("Select Territory...").SetOnClick(TargetTerritoryClicked);
-    UI.CreateLabel(row1).SetText("Current turn number: " .. turnNumber);
   end
 
 end
 
-function territoryIsNeutral(territoryDetail)
-   return territoryDetail.IsNeutral;
+function TargetTerritoryClicked()
+	local options = map(filter(Game.LatestStanding.Territories, function(t) return t.IsNeutral end), TerritoryButton);
+	UI.PromptFromList("Select the territory you'd like to take armies from", options);
 end
 
-function TargetTerritoryClicked()
-	UI.PromptFromList("Select the territory to Airdrop to", openTerritories);
+function TerritoryButton(terr)
+	local name = Game.Map.Territories[terr.ID].Name;
+	local ret = {};
+	ret["text"] = name;
+	ret["selected"] = function()
+		TargetTerritoryBtn.SetText(name);
+		TargetTerritoryID = terr.ID;
+
+		CheckCreateFinalStep();
+	end
+	return ret;
 end
+
+function CheckCreateFinalStep()
+	if (SubmitBtn == nil) then
+
+		local row3 = UI.CreateHorizontalLayoutGroup(vert);
+		UI.CreateLabel(row3).SetText(game.Game.TurnNumber .. " armies will be airdropped");
+
+		SubmitBtn = UI.CreateButton(vert).SetText("Gift").SetOnClick(SubmitClicked);
+	end
+end
+
+function SubmitClicked()
+	local msg = 'Airdropping ' .. game.Game.TurnNumber .. ' armies';
+  print(msg)
+
+	--local payload = 'GiftArmies_' .. NumArmiesInput.GetValue() .. ',' .. TargetTerritoryID .. ',' .. TargetPlayerID;
+
+	--local orders = Game.Orders;
+	--table.insert(orders, WL.GameOrderCustom.Create(Game.Us.ID, msg, payload));
+	--Game.Orders = orders;
+end
+
